36e86f249199ae4f6238f0441c7e7818f748e93f
Madeline <madelinelessard@comcast.net>
2026-06-23 13:27:15 -0700

    adding angular distance function

--- star_run.py
+++ star_run.py
@@ -2,10 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def angular_dist(ra1, dec1, ra2, dec2):
+     '''
+     #Calculates the angular distance (great circle portion) between 2 ra/dec coordinate pairs inside celestial sphere
+    #Takes in RA and Dec as decimals
+    #Angular distance returned as a decimal
+     '''
+     
+     ra1 = np.radians(ra1)
+     dec1 = np.radians(dec1)
+     ra2 = np.radians(ra2)
+     dec2 = np.radians(dec2)
+     
+     a = (np.sin(abs(dec1-dec2)/2))**2
+     b = np.cos(dec1)*np.cos(dec2)*((np.sin(abs(ra1-ra2)/2))**2)
+     d = 2*np.arcsin(np.sqrt(a+b))
+     
+     d = np.degrees(d)
+     
+     return d
+
 
 # read in csv data
 data = pd.read_csv('exoplanet_archive_2026_06_22.csv', skiprows=28)
-
 
 
 class StarSystem():
